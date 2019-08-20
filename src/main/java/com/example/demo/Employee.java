04591f6e13e995bf442b0dba0e0cf0ecc3cd4d88
bavatar <bavatar@hotmail.com>
2019-08-20 10:59:59 -0400

    employee database

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String SSN;
+    private String DOB;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
